c5ec34b9bd46b62cdceae16ac7765d41da830f1d
Akira Nishimura <akira.nishimura.711@gmail.com>
2023-05-28 02:22:50 +0900

    Thread test application

---
+++ src/mixins/globalMethods.js
@@ -0,0 +1,13 @@
+export default {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        countUp() {
+            this.counter++
+            return this.counter
+        }
+    }
+}
---
+++ src/views/AboutView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="about">
+    <div>
+      <h1>This is an about page</h1>
+    </div>
+    <br />
+    <div>Counter: {{ myCounter }} ({{ beforeCounter }})</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'aboutPage',
+  data() {
+    return {
+      myCounter: 0,
+      beforeCounter: 0
+    }
+  },
+  mounted() {
+    this.runTask()
+  },
+  methods: {
+    runTask() {
+      const ms = parseInt(Math.random() * 400 + 100)
+      setTimeout(
+        function () {
+          this.myCounter = this.countUp()
+          if (this.myCounter - this.beforeCounter === 1) {
+            this.beforeCounter = this.myCounter
+            this.runTask()
+          } else {
+            this.myCounter = `[!!!] Stop: ${this.myCounter} != ${this.beforeCounter}`
+            console.log(this.myCounter)
+          }
+        }.bind(this),
+        ms
+      )
+    }
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
